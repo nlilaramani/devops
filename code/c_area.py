7bf39c09a0d34eabe5fc98cd3c175bd07fd494ca
nlilaramani <6720026+nlilaramani@users.noreply.github.com>
2022-09-13 19:39:49 -0500

    Update c_area.py

--- code/c_area.py
+++ code/c_area.py
@@ -1,5 +1,5 @@
 #This line takes radius from user
 r=float(input("Enter radius of a circle:"))
 #This line calculates area pi=3.1
-area=round(3.14*r**2,2) #This is inline comment
+area=round(3.14*r+2,2) #This is inline comment
 print("Area of circle is:",area)
